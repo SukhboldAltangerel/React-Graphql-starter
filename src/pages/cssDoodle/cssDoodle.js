706318b0e1dcb64672c9558f9da50106797613ed
Sukhbold Altangerel <col.de.staz@gmail.com>
2021-11-30 18:38:24 +0800

    .

--- src/pages/cssDoodle/cssDoodle.js
+++ src/pages/cssDoodle/cssDoodle.js
@@ -1,18 +1,30 @@
+import { useRef } from 'react'
 import styles from './cssDoodle.module.css'
 import 'css-doodle'
 
 export default function CssDoodle() {
+   const doodleRef = useRef()
+
+   function updateDoodle() {
+      doodleRef.current.update()
+   }
+
    return (
-      <div className="">
+      <div className={styles.container} onClick={updateDoodle}>
          Css doodle
-         <css-doodle grid="5">
+         <css-doodle ref={doodleRef}>
             {`
-            :doodle {
-               grid - gap: 1px;
-               width: 8em;
-               height: 8em;
-            }
-            background: #60569e;
+               :doodle {
+                  @grid: 20 / 100vmax;
+                  background: #0a0c27;
+                  font-family: sans-serif;
+                  overflow: hidden;
+               }
+               :after {
+                  content: \@hex.@r(0x2500, 0x257f);
+                  color: hsla(@r360, 70%, 70%, @r.9);
+                  font-size: 8vmin;
+               }
             `}
          </css-doodle>
       </div>
