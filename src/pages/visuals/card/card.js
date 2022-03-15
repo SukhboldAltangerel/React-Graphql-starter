b02f9d6d749af26b9c968b5f07152e36efe95358
Sukhbold Altangerel <col.de.staz@gmail.com>
2022-03-15 18:42:55 +0800

    .

--- src/pages/visuals/card/card.js
+++ src/pages/visuals/card/card.js
@@ -1,5 +1,5 @@
 import styles from './card.module.css'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 const imgUrl = 'https://ub1.cdn.mplus.mn/r_md_h/images/publication/covers/52355/5f1556f2_a83a24_0.68.jpg'
 
@@ -9,10 +9,35 @@ export default function Card() {
       y: null
    })
 
+   const [moved, setMoved] = useState(false)
+
+   const imgRef = useRef()
+
+   function imgClick() {
+      const animation = imgRef.current.animate([{
+         transform: `translateX(${moved ? 0 : '200px'})`,
+         opacity: moved ? 1 : 0
+      }], {
+         duration: 300,
+         fill: 'forwards'
+      })
+      if (moved) {
+         imgRef.current.style.display = 'block'
+      } else {
+         animation.addEventListener('finish', () => {
+            imgRef.current.style.display = 'none'
+         })
+      }
+      setMoved(prev => !prev)
+   }
+
    return (
       <div className={styles.cardContainer}>
+         <button onClick={imgClick}>
+            Toggle
+         </button>
          <div className={styles.imageContainer}>
-            <img className={styles.image} src={imgUrl} alt='card' />
+            <img className={styles.image} src={imgUrl} alt='card' ref={imgRef} />
          </div>
       </div>
    )
